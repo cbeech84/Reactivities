7582735273810f079bb2ae29a21f595cae9b9366
Christopher Beech <Christopher.Beech@kantar.com>
2020-05-28 21:14:01 +0100

    End Section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import { Header, Icon, List } from 'semantic-ui-react'
 import './App.css';
 import axios from 'axios'
 
@@ -11,26 +11,31 @@ class App extends Component {
   componentDidMount() { //this will set the state with these values when the component is mounted
     axios.get('http://localhost:5000/api/values')
     .then((response) => {
-      console.log(response);
       this.setState({
         values: response.data
       })
     })
-    // this.setState({
+    // this.setState({ //we can use this when we just want to set static data - api solution is above
     //   values: [{id: 1, name: 'Value 101'}, {id:2, name: 'Value 102'}]
     // })
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => ( 
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+          {/* <ul> //this entire list is replaced by the list semantic UI list above
             {this.state.values.map((value: any) => ( //mapping values using a callback function to the list below - we are turning off type safety by giving it the any type.
               <li key={value.id}>{value.name}</li> //important to always provide a key value, otherwise React gets annoyed with you
             ))}
-          </ul>
-        </header>
+          </ul> */}
       </div>
     );
   }
