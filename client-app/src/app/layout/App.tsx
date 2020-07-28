6124d4e3118c035f063b1c1bc54d783e5482ac46
Christopher Beech <Christopher.Beech@kantar.com>
2020-07-28 13:07:31 +0100

    End Section 17 (client side images)

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,18 +1,19 @@
 import React, { Fragment, useContext, useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import NavBar from '../../features/nav/NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {observer} from 'mobx-react-lite';
 import { Route, withRouter, RouteComponentProps, Switch } from 'react-router-dom';
+import { RootStoreContext } from '../stores/rootStore';
+import {ToastContainer} from 'react-toastify';
+import NavBar from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import ProfilePage from '../../features/profiles/ProfilePage';
 import NotFound from './NotFound';
-import LoginForm from '../../features/user/LoginForm';
-import {ToastContainer} from 'react-toastify';
 import ModalContainer from '../common/modals/ModalContainer';
-import { RootStoreContext } from '../stores/rootStore';
 import LoadingComponent from './LoadingComponent';
+
 
 const App: React.FC<RouteComponentProps> = ({location}) => {
   const rootStore = useContext(RootStoreContext);
@@ -46,7 +47,7 @@ const App: React.FC<RouteComponentProps> = ({location}) => {
               path={['/createActivity','/manage/:id']}
               component={ActivityForm}
             />
-            <Route path='/login' component={LoginForm}/>
+            <Route path='/profile/:username' component={ProfilePage}/>
             <Route component={NotFound}/>
             </Switch>
           </Container>
--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
-import {Item, Button, Segment, Icon, Label} from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
+import { Item, Button, Segment, Icon, Label } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import { IActivity } from '../../../app/models/activity';
-import {format} from 'date-fns';
-import {ActivityListItemAttendees} from '../dashboard/ActivityListItemAttendees'
+import { format } from 'date-fns';
+import { ActivityListItemAttendees } from '../dashboard/ActivityListItemAttendees'
 
-const ActivityListItem : React.FC<{activity: IActivity}>= ({activity}) => {
+const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
   const host = activity.attendees.filter(x => x.isHost)[0];
   return (
     <Segment.Group>
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size='tiny' circular src={host.image || '/assets/user.png'} />
+            <Item.Image size='tiny' circular src={host.image || '/assets/user.png'} style={{ marginBottom: 3 }} />
             <Item.Content>
               <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
               <Item.Description>
-                Hosted by {host.displayName}
+                Hosted by
+                <Link to={`/profile/${host.username}`}> {host.displayName}</Link>
               </Item.Description>
-              {activity.isHost && 
+              {activity.isHost &&
                 <Item.Description>
                   <Label basic color='orange' content='You are hosting this activity' />
                 </Item.Description>
               }
-              {activity.isGoing && !activity.isHost && 
+              {activity.isGoing && !activity.isHost &&
                 <Item.Description>
                   <Label basic color='orange' content='You are going to this activity' />
                 </Item.Description>
@@ -33,23 +34,23 @@ const ActivityListItem : React.FC<{activity: IActivity}>= ({activity}) => {
         </Item.Group>
       </Segment>
       <Segment>
-        <Icon name='clock'/> {format(activity.date, 'h:mm a')}
-        <Icon name='marker'/> {activity.venue}, {activity.city}
+        <Icon name='clock' /> {format(activity.date, 'h:mm a')}
+        <Icon name='marker' /> {activity.venue}, {activity.city}
       </Segment>
       <Segment secondary>
-        <ActivityListItemAttendees attendees={activity.attendees}/>
+        <ActivityListItemAttendees attendees={activity.attendees} />
       </Segment>
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
           as={Link} to={`/activities/${activity.id}`}
-          floated='right' 
-          content='View' 
-          color='blue' 
+          floated='right'
+          content='View'
+          color='blue'
         />
       </Segment>
     </Segment.Group>
-    
+
   )
 }
 
